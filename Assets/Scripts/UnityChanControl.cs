337e90480a90e49d0f6c0e57775c0344d450ea11
Juan Acevedo <entrpn@gmail.com>
2018-10-21 12:58:50 -0700

    First Commit

---
+++ Assets/Scripts/UnityChanControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityControl : MonoBehaviour {
+
+    public float animSpeed = 1.5f;
+
+    public float forwardSpeed = 7.0f;
+
+    public float backwardSpeed = 2.0f;
+
+    public float rotateSpeed = 2.0f;
+
+    public float jumpPower = 3.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
